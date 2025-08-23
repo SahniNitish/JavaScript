da67cbeb1a5098ec4970f6b6b15512be2fea8c0a
Nitish sahni <nitish@realmeta.ca>
2025-08-23 15:09:38 -0300

    classes&Constructor

--- object&classes/script.js
+++ object&classes/script.js
@@ -1,14 +1,41 @@
-const employee = {
-    calcTax(){
-        console.log("Tax Rate is 10%");
+//__prototype in JS 
+// const employee = {
+//     calcTax(){
+//         console.log("Tax Rate is 10%");
+//     }
+// };
+
+// const karanVeer = {
+//     salary: 50000,
+//     calcTax(){
+//         console.log("Tax Rate is 20% ");
+//     }
+// };
+
+// karanVeer.__proto__ = employee ;
+
+//
+//Classes in Js 
+//Class is a  program-code template for creating Objects 
+//Those objects wil have some state (variables) & some behaviour(functions) inside it .
+
+class ToyotaCar{
+    Start(){
+    console.log("start");
+    };
+
+    stop(){
+        console.log("stop");
+    };
+    setBrand(brand){
+        this.setBrand = brand;
     }
 };
 
-const karanVeer = {
-    salary: 50000,
-    calcTax(){
-        console.log("Tax Rate is 20% ");
-    }
-};
+let fortuner = new ToyotaCar();
+fortuner.setBrand = ("fortuner");
 
-karanVeer.__proto__ = employee ;
+let lexus = new ToyotaCar();
+lexus.setBrand = ("Lexus");
+
+//Constructor() Method is 
